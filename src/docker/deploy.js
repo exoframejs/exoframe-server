ab8a2c33bd293780fdb47de8aaaa8eb07f5c037c
Tim Ermilov <yamalight@gmail.com>
2016-08-29 14:43:27 +0200

    Add basic deploy command

---
+++ src/docker/deploy.js
@@ -0,0 +1,55 @@
+// npm packages
+import uuid from 'node-uuid';
+
+// our packages
+import logger from '../logger';
+import docker from './docker';
+import {checkAuth} from '../auth/chechAuth';
+
+export default (app) => {
+  app.post('/api/deploy', checkAuth, async (req, res) => {
+    logger.debug(req.body);
+    const {deployId, services: reqServices} = req.body;
+    const deployLabel = deployId || uuid.v1();
+    if (!reqServices || !reqServices.length) {
+      res.sendStatus(400);
+      return;
+    }
+
+    // get existing user images
+    const allImages = await docker.listImagesAsync();
+    const images = allImages.filter(img => img.Labels && img.Labels['exoframe.user'] === req.userInfo.username);
+    logger.debug('Got user images:', images);
+
+    // get required services
+    const services = reqServices.map(service => {
+      const image = images.find(svc => svc.RepoTags[0].indexOf(service.name) !== -1);
+      return {
+        ...service,
+        image,
+      };
+    });
+    logger.debug('Got services:', services);
+
+    // create containers
+    const containers = await Promise.all(services.map(svc => {
+      const cfg = {
+        Image: `${svc.image.RepoTags[0]}`,
+        name: `exo-${svc.name}-${deployLabel}`,
+        Labels: {
+          'exoframe.deployment': `ex-pipeline-${deployLabel}`,
+        },
+      };
+
+      if (svc.env) {
+        cfg.Env = svc.env;
+      }
+
+      return docker.createContainerAsync(cfg);
+    }));
+    // start containers
+    await Promise.all(containers.map(container => container.startAsync()));
+
+    res.send(containers);
+  });
+};
--- src/docker/index.js
+++ src/docker/index.js
@@ -1,7 +1,9 @@
 import build from './build';
 import list from './list';
+import deploy from './deploy';
 
 export default (app) => {
   build(app);
   list(app);
+  deploy(app);
 };
