48712acba3f6cb5162e555df843455c72c172854
Tim Ermilov <yamalight@gmail.com>
2016-08-31 14:19:32 +0200

    Add loggin for build path

--- src/docker/build.js
+++ src/docker/build.js
@@ -1,6 +1,7 @@
 // npm packages
 
 // our packages
+import logger from '../logger';
 import docker from './docker';
 import {checkAuth} from '../auth/chechAuth';
 
@@ -8,6 +9,7 @@ export default (app) => {
   app.post('/api/build', checkAuth, async (req, res) => {
     const {tag, labels} = req.query;
     const labelsParsed = JSON.parse(labels);
+    logger.info('building image with:', {tag, labelsParsed});
     const output = await docker.buildImageAsync(req, {t: tag, labels: labelsParsed});
     output.pipe(res);
   });
