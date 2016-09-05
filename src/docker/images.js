9747132a398757ab8d9620ae242ded1beb73bfb8
Tim Ermilov <yamalight@gmail.com>
2016-09-05 13:28:42 +0200

    Allow listing images that were fetched from repos

--- src/docker/images.js
+++ src/docker/images.js
@@ -3,11 +3,18 @@
 // our packages
 import docker from './docker';
 import {checkAuth} from '../auth/chechAuth';
+import logger from '../logger';
 
 export default (app) => {
   app.get('/api/images', checkAuth, async (req, res) => {
+    const {public: includePublic} = req.query;
     const allImages = await docker.listImagesAsync();
-    const images = allImages.filter(img => img.Labels && img.Labels['exoframe.user'] === req.userInfo.username);
+    logger.info('Getting images:', includePublic);
+    const images = allImages.filter(img =>
+      (includePublic && img.RepoDigests && img.RepoDigests.length > 0) ||
+      (img.Labels && img.Labels['exoframe.user'] === req.userInfo.username)
+    );
+    logger.debug('Got images:', images);
     res.send(images);
   });
 };
