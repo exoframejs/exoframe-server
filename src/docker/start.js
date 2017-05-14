52ed981362eed642b94ece3d21c2a744fd28e174
Tim Ermilov <yamalight@gmail.com>
2017-05-14 13:37:10 +0200

    Add support for custom domains and env vars to deployments

--- src/docker/start.js
+++ src/docker/start.js
@@ -3,21 +3,34 @@ const uuid = require('uuid');
 
 // our modules
 const docker = require('./docker');
+const {getProjectConfig} = require('./util');
 
 module.exports = async ({image, username}) => {
   const baseName = image.split(':').shift();
   const uid = uuid.v1();
   const name = `${baseName}-${uid.split('-').shift()}`;
 
+  // get project info
+  const config = getProjectConfig();
+
+  // generate host
+  const host = config.domain || 'localhost';
+
+  // generate env vars
+  const Env = config.env
+    ? Object.keys(config.env).map(key => `${key}=${config.env[key]}`)
+    : [];
+
   // create container
   const container = await docker.createContainer({
     Image: image,
     name,
+    Env,
     Labels: {
       'exoframe.deployment': name,
       'exoframe.user': username,
       'traefik.backend': baseName,
-      'traefik.frontend.rule': 'Host:localhost',
+      'traefik.frontend.rule': `Host:${host}`,
     },
     HostConfig: {
       RestartPolicy: {
