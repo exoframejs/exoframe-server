92f038eb3234646581472caade66ad7283ccf29d
Tim Ermilov <yamalight@gmail.com>
2017-06-29 11:49:29 +0200

    Fix node dockerfile and use npm to start app

--- test/logs.js
+++ test/logs.js
@@ -30,7 +30,22 @@ module.exports = (server, token, name) =>
             const parts = line.split(/\dZ\s/);
             return parts[1].replace(/\sv\d.+/, ''); // strip any versions
           });
-        t.deepEqual(lines, ['yarn start', '$ node index.js ', 'Listening on port 80'], 'Should have log');
+        t.deepEqual(
+          lines,
+          [
+            'npm info it worked if it ends with ok',
+            'npm info using npm@4.2.0',
+            'npm info using node@v7.10.0',
+            'npm info lifecycle node-project@1.0.0~prestart: node-project@1.0.0',
+            'npm info lifecycle node-project@1.0.0~start: node-project@1.0.0',
+            '',
+            '> node-project@1.0.0 start /usr/src/app',
+            '> node index.js',
+            '',
+            'Listening on port 80',
+          ],
+          'Should have correct log'
+        );
 
         t.end();
       });
