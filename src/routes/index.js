efde401482f4a438326e5d1fd56428b7a395fcde
Tim Ermilov <yamalight@gmail.com>
2017-07-06 19:27:20 +0200

    Add home route that links to github

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,12 @@ const deploy = require('./deploy');
 const list = require('./list');
 const remove = require('./remove');
 const logs = require('./logs');
+const home = require('./home');
 
 module.exports = server => {
   deploy(server);
   list(server);
   remove(server);
   logs(server);
+  home(server);
 };
