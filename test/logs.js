d3773c040f5251c1ce7e75e4dc25d6ace63a92fb
Tim Ermilov <yamalight@gmail.com>
2017-08-02 10:58:56 +0200

    Use fulltext matching for project logs test to fix CI fails

--- test/logs.js
+++ test/logs.js
@@ -66,7 +66,7 @@ module.exports = (server, token, data) =>
         t.equal(response.statusCode, 200, 'Correct status code');
 
         const {result} = response;
-        const lines = result
+        const text = result
           // split by lines
           .split('\n')
           // remove unicode chars
@@ -80,19 +80,17 @@ module.exports = (server, token, data) =>
             }
             const parts = line.split(/\dZ\s/);
             return parts[1].replace(/\sv\d.+/, ''); // strip any versions
-          });
+          })
+          .join();
 
-        t.ok(lines[0].startsWith('Logs for exo-admin-test-compose-deploy-redis-'));
-        t.ok(lines[1].includes('oO0OoO0OoO0Oo Redis is starting oO0OoO0OoO0Oo'));
-        t.ok(lines[2].includes('Redis version=') && lines[2].includes('just started'));
-        t.ok(
-          lines[3].includes(
-            'Warning: no config file specified, using the default config. In order to specify a config file use redis-server /path/to/redis.conf'
-          )
-        );
-        t.ok(lines[4].includes('Running mode=standalone, port=6379.'));
-        t.ok(lines[lines.length - 2].includes('Ready to accept connections'));
-        t.ok(lines[lines.length - 1].startsWith('Logs for exo-admin-test-compose-deploy-web-'));
+        t.ok(text.includes('Logs for exo-admin-test-compose-deploy-redis-'));
+        t.ok(text.includes('oO0OoO0OoO0Oo Redis is starting oO0OoO0OoO0Oo'));
+        t.ok(text.includes('Redis version='));
+        t.ok(text.includes('just started'));
+        t.ok(text.includes('Warning: no config file specified, using the default config.'));
+        t.ok(text.includes('Running mode=standalone, port=6379.'));
+        t.ok(text.includes('Ready to accept connections'));
+        t.ok(text.includes('Logs for exo-admin-test-compose-deploy-web-'));
 
         t.end();
       });
