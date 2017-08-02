a6c41cc31eb32c8853c02629f411b5d8482a5e1f
Tim Ermilov <yamalight@gmail.com>
2017-08-02 10:39:09 +0200

    Allow getting logs for whole projects, not just single deployments

--- src/routes/logs.js
+++ src/routes/logs.js
@@ -1,5 +1,15 @@
 // our modules
+const _ = require('highland');
 const docker = require('../docker/docker');
+
+const logsConfig = {
+  follow: false,
+  stdout: true,
+  stderr: true,
+  timestamps: true,
+};
+
+const cleanLogs = text => text.replace(/^\u0001.+?\d/, '').replace(/\n+$/, '');
 
 module.exports = server => {
   server.route({
@@ -18,19 +28,37 @@ module.exports = server => {
         c => c.Labels['exoframe.user'] === username && c.Names.find(n => n === `/${id}`)
       );
 
-      if (!containerInfo) {
+      if (containerInfo) {
+        const container = docker.getContainer(containerInfo.Id);
+        const logStream = await container.logs(logsConfig);
+        reply(null, logStream);
+        return;
+      }
+
+      // if not found by name - try to find by project
+      const containers = allContainers.filter(
+        c => c.Labels['exoframe.user'] === username && c.Labels['exoframe.project'] === id
+      );
+      if (!containers.length) {
         reply({error: 'Container not found!'}).code(404);
         return;
       }
 
-      const container = docker.getContainer(containerInfo.Id);
-      const logStream = await container.logs({
-        follow: false,
-        stdout: true,
-        stderr: true,
-        timestamps: true,
-      });
-      reply(null, logStream);
+      // get all log streams and prepend them with service names
+      const logRequests = await Promise.all(
+        containers.map(async cInfo => {
+          const container = docker.getContainer(cInfo.Id);
+          const logs = await container.logs(logsConfig);
+          const name = cInfo.Names[0].replace(/^\//, '');
+          const nameStream = _([`Logs for ${name}\n\n`]);
+          return [nameStream, _(logs)];
+        })
+      );
+      // flatten results
+      const allLogsStream = _(logRequests).flatten();
+      // get raw response from request to work around hapi issues with highland streams
+      const response = request.raw.res;
+      allLogsStream.pipe(response);
     },
   });
 };
--- test/logs.js
+++ test/logs.js
@@ -3,7 +3,7 @@ const tap = require('tap');
 
 module.exports = (server, token, data) =>
   new Promise(resolve => {
-    tap.test('Should get logs for current project', t => {
+    tap.test('Should get logs for current deployment', t => {
       // options base
       const options = {
         method: 'GET',
@@ -13,7 +13,7 @@ module.exports = (server, token, data) =>
         },
       };
 
-      server.inject(options, async response => {
+      server.inject(options, response => {
         // check response
         t.equal(response.statusCode, 200, 'Correct status code');
 
@@ -51,6 +51,66 @@ module.exports = (server, token, data) =>
       });
     });
 
+    tap.test('Should get logs for current project', t => {
+      // options base
+      const options = {
+        method: 'GET',
+        url: `/logs/${encodeURIComponent(data.project)}`,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+
+      server.inject(options, response => {
+        // check response
+        t.equal(response.statusCode, 200, 'Correct status code');
+
+        const {result} = response;
+        const lines = result
+          // split by lines
+          .split('\n')
+          // remove unicode chars
+          .map(line => line.replace(/^\u0001.+?\d/, '').replace(/\n+$/, ''))
+          // filter blank lines
+          .filter(line => line && line.length > 0)
+          // remove timestamps
+          .map(line => {
+            if (line.startsWith('Logs for')) {
+              return line;
+            }
+            const parts = line.split(/\dZ\s/);
+            return parts[1].replace(/\sv\d.+/, ''); // strip any versions
+          });
+
+        console.log(lines);
+
+        t.ok(lines[0].startsWith('Logs for exo-admin-test-compose-deploy-redis-'));
+        t.ok(lines[1].includes('oO0OoO0OoO0Oo Redis is starting oO0OoO0OoO0Oo'));
+        t.ok(lines[2].includes('Redis version=') && lines[2].includes('just started'));
+        t.ok(
+          lines[3].includes(
+            'Warning: no config file specified, using the default config. In order to specify a config file use redis-server /path/to/redis.conf'
+          )
+        );
+        t.ok(lines[4].includes('Running mode=standalone, port=6379.'));
+        t.ok(
+          lines[5].includes(
+            'WARNING: The TCP backlog setting of 511 cannot be enforced because /proc/sys/net/core/somaxconn is set to the lower value of 128.'
+          )
+        );
+        t.ok(lines[6].includes('Server initialized'));
+        t.ok(
+          lines[7].includes(
+            `WARNING you have Transparent Huge Pages (THP) support enabled in your kernel. This will create latency and memory usage issues with Redis. To fix this issue run the command 'echo never > /sys/kernel/mm/transparent_hugepage/enabled' as root, and add it to your /etc/rc.local in order to retain the setting after a reboot. Redis must be restarted after THP is disabled.`
+          )
+        );
+        t.ok(lines[8].includes('Ready to accept connections'));
+        t.ok(lines[9].startsWith('Logs for exo-admin-test-compose-deploy-web-'));
+
+        t.end();
+      });
+    });
+
     tap.test('Should not get logs for nonexistent project', t => {
       // options base
       const options = {
@@ -61,7 +121,7 @@ module.exports = (server, token, data) =>
         },
       };
 
-      server.inject(options, async response => {
+      server.inject(options, response => {
         // check response
         t.equal(response.statusCode, 404, 'Correct status code');
         t.deepEqual(response.result, {error: 'Container not found!'}, 'Should have error');
