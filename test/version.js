6a486f1f6d8b8f152e397e82b3f041746921da74
Tim Ermilov <yamalight@gmail.com>
2017-09-18 13:54:56 +0200

    Add route that gets and displays current and latest versions of server and traefik

---
+++ test/version.js
@@ -0,0 +1,25 @@
+// npm packages
+const tap = require('tap');
+
+module.exports = (server, token) => {
+  tap.test('Should get current and latest versions', t => {
+    // options base
+    const options = {
+      method: 'GET',
+      url: '/version',
+    };
+
+    server.inject(options, async response => {
+      const result = response.result;
+
+      // check response
+      t.equal(response.statusCode, 200, 'Correct status code');
+      t.ok(result.server, 'Should have server version');
+      t.ok(result.traefik, 'Should have traefik version');
+      t.ok(result.latestServer, 'Should have latest server version');
+      t.ok(result.latestTraefik, 'Should have latest traefik version');
+
+      t.end();
+    });
+  });
+};
