21c7ad3e939eee0d70b6ed0e051d3f5e2cc97cc0
Tim Ermilov <yamalight@gmail.com>
2018-07-10 13:08:08 +0200

    Update dependencies

--- src/logger/index.js
+++ src/logger/index.js
@@ -1,18 +1,20 @@
 const winston = require('winston');
 
+// get format functions
+const {
+  format: {combine, prettyPrint, timestamp, label, colorize, printf},
+} = winston;
+
+// prepare level
 const levelTesting = process.env.NODE_ENV === 'testing' ? 'error' : false;
 const level = levelTesting || process.env.NODE_ENV === 'production' ? 'info' : 'debug';
 
-const logger = new winston.Logger({
-  transports: [
-    new winston.transports.Console({
-      level,
-      colorize: true,
-      timestamp: true,
-      prettyPrint: true,
-      label: 'exoframe-server',
-    }),
-  ],
+// log formatting function
+const myFormat = printf(info => `${info.timestamp} [${info.label}] ${info.level}: ${info.message}`);
+
+const logger = winston.createLogger({
+  format: combine(timestamp(), colorize(), prettyPrint(), label({label: 'exoframe-server'}), myFormat),
+  transports: [new winston.transports.Console({level})],
 });
 
 module.exports = logger;
