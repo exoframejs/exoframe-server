592d4cee41eb6c3a265eb1df12b287cb4f943677
Tim Ermilov <yamalight@gmail.com>
2018-07-10 14:03:26 +0200

    Replace winston with signale

--- src/logger/index.js
+++ src/logger/index.js
@@ -1,20 +1,22 @@
-const winston = require('winston');
-
-// get format functions
-const {
-  format: {combine, prettyPrint, timestamp, label, colorize, printf},
-} = winston;
+const {Signale} = require('signale');
 
 // prepare level
 const levelTesting = process.env.NODE_ENV === 'testing' ? 'error' : false;
 const level = levelTesting || process.env.NODE_ENV === 'production' ? 'info' : 'debug';
 
-// log formatting function
-const myFormat = printf(info => `${info.timestamp} [${info.label}] ${info.level}: ${info.message}`);
-
-const logger = winston.createLogger({
-  format: combine(timestamp(), colorize(), prettyPrint(), label({label: 'exoframe-server'}), myFormat),
-  transports: [new winston.transports.Console({level})],
+const logger = new Signale({
+  scope: 'exoframe-server',
+  types: {
+    debug: {
+      stream: level === 'debug' ? [process.stdout] : [],
+    },
+    info: {
+      stream: level === 'info' ? [process.stdout] : [],
+    },
+    warn: {
+      stream: level === 'info' ? [process.stdout] : [],
+    },
+  },
 });
 
 module.exports = logger;
