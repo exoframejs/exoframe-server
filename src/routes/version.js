cf88719fdbe1d3fbb82d66d00216dd2016980a35
Tim Ermilov <yamalight@gmail.com>
2018-07-23 13:02:27 +0200

    Catch errors in version method

--- src/routes/version.js
+++ src/routes/version.js
@@ -21,25 +21,29 @@ module.exports = fastify => {
     method: 'GET',
     path: '/version',
     async handler(request, reply) {
-      // get version of traefik
-      let traefikVersion = 'none';
-      const allImages = await docker.listImages();
-      const traefik = allImages.find(img => img.RepoTags && img.RepoTags.find(t => t.includes('traefik')));
-      if (traefik) {
-        traefikVersion = traefik.Labels['org.label-schema.version'];
+      try {
+        // get version of traefik
+        let traefikVersion = 'none';
+        const allImages = await docker.listImages();
+        const traefik = allImages.find(img => img.RepoTags && img.RepoTags.find(t => t.includes('traefik')));
+        if (traefik) {
+          traefikVersion = traefik.Labels['org.label-schema.version'];
+        }
+        // get latest versions
+        const lastServerTag = await getLatestVersion(exoServerUrl);
+        const lastTraefikTag = await getLatestVersion(traefikUrl);
+        // reply
+        reply.code(200).send({
+          server: pkg.version,
+          latestServer: lastServerTag,
+          serverUpdate: cmp(lastServerTag, pkg.version) > 0,
+          traefik: traefikVersion,
+          latestTraefik: lastTraefikTag,
+          traefikUpdate: cmp(lastTraefikTag, traefikVersion) > 0,
+        });
+      } catch (error) {
+        reply.code(500).send({error});
       }
-      // get latest versions
-      const lastServerTag = await getLatestVersion(exoServerUrl);
-      const lastTraefikTag = await getLatestVersion(traefikUrl);
-      // reply
-      reply.code(200).send({
-        server: pkg.version,
-        latestServer: lastServerTag,
-        serverUpdate: cmp(lastServerTag, pkg.version) > 0,
-        traefik: traefikVersion,
-        latestTraefik: lastTraefikTag,
-        traefikUpdate: cmp(lastTraefikTag, traefikVersion) > 0,
-      });
     },
   });
 };
